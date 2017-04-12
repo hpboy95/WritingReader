3d0190722906823daf1326f4b2e0930b9981a1e7
Guillermo-C <ninjamemo@guillermos-macbook-pro.local>
2017-04-11 20:14:41 -0700

    Successfully connected Firebase to the Writing Reader app.

--- WritingReader/WritingReader/ViewController.swift
+++ WritingReader/WritingReader/ViewController.swift
@@ -11,22 +11,27 @@ import Firebase
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = FIRDatabase.database().reference()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var ref:FIRDatabaseReference!
+    
+    
     @IBAction func signIn(_ sender: UIButton) {
-        
+        ref.child("Email").setValue(emailTextField.text!)
     }
     
     
